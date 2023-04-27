5f8ef7f73425ecccbc657af6d5972dfe291bef8d
João Victor da Silva <jv147002@gmail.com>
2023-04-27 16:51:32 -0300

    aprendi rotas

---
+++ js/router.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const routes = {
+    '/' : '/pages/home.html',
+    '/vermelho' : '/pages/vermelho.html',
+    '/azul' : '/pages/azul.html',
+    '/verde' : '/pages/green.html',
+    '/tomato' : '/pages/tomato.html',
+}
+
+const route = async () => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+    
+    const response = await fetch(routes[path])
+    const html = await response.text()
+
+    document.getElementById('root').innerHTML = html
+}
+
+window.route = route
